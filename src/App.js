5971ce0f09bea781d191524cca7baa7ccd3def89
Jhonny Lima <jlima@Jhonnys-MacBook-Pro.local>
2020-07-29 17:29:04 -0300

    updated App.js with new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import Menu from './components/Menu'
+import initialData from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
+    <div style={{ background: "#141414" }}>
       <Menu />
+
+      <BannerMain 
+        videoTitle={initialData.categorias[0].videos[0].titulo}
+        url={initialData.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área, os termos HTML, CSS e JavaScript fazem parte da rotina dos desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={initialData.categorias[0]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={initialData.categorias[1]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={initialData.categorias[2]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={initialData.categorias[3]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={initialData.categorias[4]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={initialData.categorias[5]}
+      />
+
+      <Footer />
+
     </div>
   );
 }
